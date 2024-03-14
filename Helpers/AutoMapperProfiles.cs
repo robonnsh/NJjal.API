0185856b90137a65b2119b7dd72481ba4baca46b
robo ninoshvili <nnshtofficial@gmail.com>
2024-03-14 18:22:07 +0400

    Add Automapper profiles for automatically map from source to Dtos

---
+++ Helpers/AutoMapperProfiles.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using Njal_back.DTOS;
+using Njal_back.Models;
+
+namespace Njal_back.Helpers
+{
+    public class AutoMapperProfiles : Profile
+    {
+        public AutoMapperProfiles()
+        {
+            // Update whole product
+            CreateMap<Product, ProductDto>().ReverseMap();
+
+            // Update only designer name
+            CreateMap<Product, DesignerNameDto>().ReverseMap();
+
+            // Update only product name
+            CreateMap<Product, ProductNameDto>().ReverseMap();
+            
+            // update price only
+            CreateMap<Product, PriceDto>().ReverseMap();
+        
+        }
+    }
+}
