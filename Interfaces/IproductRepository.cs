48642c4e24552fbd5135adac24269f7317cf58b1
robo ninoshvili <nnshtofficial@gmail.com>
2024-03-14 18:17:04 +0400

    Add Interface for product repository

--- Interfaces/IproductRepository.cs
+++ Interfaces/IproductRepository.cs
@@ -1,11 +1,33 @@
-﻿using Njal_back.Models;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.JsonPatch;
+using Njal_back.DTOS;
+using Njal_back.Models;
 
 namespace Njal_back.Interfaces
 {
     public interface IproductRepository
     {
+        // Get
         Task<IEnumerable<Product>> GetProductsAsync();
+        // Post
         void AddProduct(Product product);
+        //Delete
         void DeleteProduct(Guid productId);
+        // Find Product
+        //Task<Product> UpdateProduct(Guid productId, Product updateProductRequest);
+        Task<Product> UpdateProduct(Guid productId, ProductDto updateProductRequest);
+
+        // Update designer name
+        Task<Product> UpdateDesignerName(Guid productId, DesignerNameDto updateDesignerNameReq);
+        
+        // Update product name 
+        Task<Product> UpdateProductName(Guid productId, ProductNameDto updateProductNameReq);
+
+
+        // Update price
+        Task<Product> UpdatePrice(Guid productId, PriceDto updatePriceReq);
+
+        // Find Patch currently not using 
+        //Task<Product> FindPatchProduct(Guid id, JsonPatchDocument<Product> updateProductRequest);
     }
 }
