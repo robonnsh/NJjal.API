133ad8ea754bba98320b70a9cbb4d5db27032d4e
robo ninoshvili <nnshtofficial@gmail.com>
2024-03-14 18:25:20 +0400

    add product repository for writing methods for product controller

--- Data/Repo/ProductRepository.cs
+++ Data/Repo/ProductRepository.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.EntityFrameworkCore;
+using Njal_back.DTOS;
 using Njal_back.Interfaces;
 using Njal_back.Models;
 
@@ -12,23 +15,74 @@ namespace Njal_back.Data.Repo
         {
             this.dc = dc;
         }
+
+        // Post
         public void AddProduct(Product product)
         {
             dc.Products.Add(product);
         }
 
+        // Delete
         public void DeleteProduct(Guid productId)
         {
             var product = dc.Products.Find(productId);
             dc.Products.Remove(product);
         }
 
-
+        // Get
         public async Task<IEnumerable<Product>> GetProductsAsync()
         {
             return await dc.Products.ToListAsync();
         }
 
-       
+        // Put - update whole product
+        public async Task<Product> UpdateProduct(Guid productId, ProductDto updateProductRequest)
+        {
+            var product = await dc.Products.FindAsync(productId);
+
+            product.DesignerName = updateProductRequest.DesignerName;
+            product.ProductName = updateProductRequest.ProductName;
+            product.Price = updateProductRequest.Price;
+
+            await dc.SaveChangesAsync();
+
+            return product;
+        }
+
+        // Put - update designer name
+        public async Task<Product> UpdateDesignerName(Guid productId, DesignerNameDto updateDesignerNameReq)
+        {
+            var designerName = await dc.Products.FindAsync(productId);
+            designerName.DesignerName = updateDesignerNameReq.DesignerName;
+            await dc.SaveChangesAsync();
+            return designerName;
+        }
+
+        // Put - update product name 
+        public async Task<Product> UpdateProductName(Guid productId, ProductNameDto updateProductNameReq)
+        {
+            var productName = await dc.Products.FindAsync(productId);
+            productName.ProductName = updateProductNameReq.ProductName;
+            await dc.SaveChangesAsync();
+            return productName;
+        }
+        public async Task<Product> UpdatePrice(Guid productId, PriceDto updatePriceReq)
+        {
+            var price = await dc.Products.FindAsync(productId);
+            price.Price = updatePriceReq.Price;
+            await dc.SaveChangesAsync();
+            return price;
+        }
+
+
+
+        // Patch currently not using 
+
+        //public async Task<Product> FindPatchProduct(Guid productId, JsonPatchDocument<Product> updateProductRequest)
+        //{
+        //    var originalProduct = await dc.Products.FindAsync(productId);
+        //    updateProductRequest.ApplyTo(originalProduct);
+        //    return originalProduct;
+        //}
     }
 }
