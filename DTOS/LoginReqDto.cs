c29d8eaf0220ce292c2bc95708da1a1a2f0abd6a
robo ninoshvili <nnshtofficial@gmail.com>
2024-03-15 23:26:16 +0400

    add login request and response DTo

---
+++ DTOS/LoginReqDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Njal_back.DTOS
+{
+    public class LoginReqDto
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ DTOS/LoginResDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Njal_back.DTOS
+{
+    public class LoginResDto
+    {
+        public string Username { get; set; }
+        public string Token { get; set; }
+    }
+}
