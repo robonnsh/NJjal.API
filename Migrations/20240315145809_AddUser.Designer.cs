cfb20021d5b3d24c8f89ad34bbcd82fe449976e5
robo ninoshvili <nnshtofficial@gmail.com>
2024-03-15 23:20:28 +0400

    add migration for users

--- Migrations/20240315145809_AddUser.Designer.cs
+++ Migrations/20240315145809_AddUser.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Njal_back.Data;
 
@@ -10,9 +11,11 @@ using Njal_back.Data;
 namespace Njal_back.Migrations
 {
     [DbContext(typeof(NjalDbContext))]
-    partial class NjalDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240315145809_AddUser")]
+    partial class AddUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20240315145809_AddUser.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Njal_back.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Usernam",
+                table: "Users",
+                newName: "Username");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Username",
+                table: "Users",
+                newName: "Usernam");
+        }
+    }
+}
