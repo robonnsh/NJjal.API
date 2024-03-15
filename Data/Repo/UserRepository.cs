639a195e6d3c62561093e0993b90826769d8ddb8
robo ninoshvili <nnshtofficial@gmail.com>
2024-03-15 23:21:27 +0400

    add user repository

---
+++ Data/Repo/UserRepository.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Njal_back.Interfaces;
+using Njal_back.Models;
+
+namespace Njal_back.Data.Repo
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly NjalDbContext dc;
+
+        public UserRepository(NjalDbContext dc)
+        {
+            this.dc = dc;
+        }
+        public async Task<User> Authenticate(string userName, string password)
+        {
+            return await dc.Users.FirstOrDefaultAsync
+                (x => x.Username == userName && x.Password == password);
+
+        }
+    }
+}
