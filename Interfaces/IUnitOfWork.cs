934adb35132dd28b089eb3e0b9f94f4665076a14
robo ninoshvili <nnshtofficial@gmail.com>
2024-03-15 23:20:52 +0400

    add interface for user repository

--- Interfaces/IUnitOfWork.cs
+++ Interfaces/IUnitOfWork.cs
@@ -3,6 +3,7 @@
     public interface IUnitOfWork
     {
         IproductRepository ProductRepository { get; }
+        IUserRepository UserRepository { get; }
         Task<bool> SaveAsync();
     }
 }
---
+++ Interfaces/IUserRepository.cs
@@ -0,0 +1,10 @@
+﻿using Njal_back.Models;
+
+namespace Njal_back.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task<User> Authenticate(string userName, string password);  
+
+    }
+}
